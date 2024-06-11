2c4dad637dc7bd39f4451c0add9d7448317e961b
alena-apr <a1-a1-a1@yandex.ru>
2024-06-11 20:04:37 +0300

    th

--- app.js
+++ app.js
@@ -43,5 +43,6 @@ console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 const collection = db.collection("users");
-count = await collection.countDocuments();
+count =  collection.countDocuments();
+// count = await collection.countDocuments();
 console.log(`В коллекции users ${count} документа/ов`);
